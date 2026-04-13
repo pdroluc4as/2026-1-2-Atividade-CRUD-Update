6255b69dca5e482cff1994ca1acffb8421260ea6
xarobi <132419016+pdroluc4as@users.noreply.github.com>
2026-04-13 17:26:44 +0000

    continuação atividade

---
+++ atividade/src/app/produtos/page.tsx
@@ -0,0 +1,18 @@
+
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Listagem de Produtos</h1>
+      <ol>
+        <Produtos></Produtos>
+      </ol>
+    </div>
+  );
+}
+
+function Produtos() {
+    return (
+        <p>Produto</p>
+    ); 
+}
