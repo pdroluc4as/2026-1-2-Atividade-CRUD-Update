d797242eae417b65b9dbba087ae574d935fc1d9c
pdroluc4as <pedrolucasoliverdesousa@gmail.com>
2026-04-13 21:10:23 -0300

    criado pagina com lista de produtos

--- atividade/src/app/produtos/page.tsx
+++ atividade/src/app/produtos/page.tsx
@@ -1,18 +1,79 @@
+'use client';
 
+import { useEffect, useState } from "react";
+import { getProdutosTodos } from "@/services/api"
+
+import { Badge } from "@/components/ui/badge"
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardAction,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+
+interface ProdutoType {
+  id: number;
+  title: string;
+  price: number;
+  description: string;
+  category: string;
+  thumbnail: string;
+}
 
 export default function Home() {
+  const [produtos, setProdutos] = useState<ProdutoType[]>([]);
+
+  useEffect(() => {
+    const carregar = async() => {
+      const resultado = await getProdutosTodos();
+      console.log(resultado.data.products);
+      setProdutos(resultado.data.products);
+    }
+    carregar();
+}, [])
+
   return (
     <div>
       <h1>Listagem de Produtos</h1>
-      <ol>
-        <Produtos></Produtos>
+      <ol className="flex flex-wrap">
+        {produtos.map(p => (
+          <CardProduto key={p.id} produto={p} />
+        ))}
       </ol>
     </div>
   );
 }
 
-function Produtos() {
+interface CardProdutoProp {
+  produto:ProdutoType
+}
+
+function CardProduto({produto}:CardProdutoProp) {
     return (
-        <p>Produto</p>
+        <Card className="relative mx-auto w-full max-w-sm pt-0">
+      <div className="absolute inset-0 z-30 aspect-video bg-black/35" />
+      <img
+        src={produto.thumbnail}
+        alt="Foto Produto"
+        className="relative z-20 aspect-video w-full object-cover"
+      />
+      <CardHeader>
+        <CardAction>
+          <Badge variant="secondary">Featured</Badge>
+        </CardAction>
+        <CardTitle>{produto.title}</CardTitle>
+        <CardDescription>
+          {produto.description}
+        </CardDescription>
+      </CardHeader>
+      <CardFooter className="flex justify-around">
+        <Button className="w-15">{produto.price}</Button>
+        <span>Editar</span>
+      </CardFooter>
+      
+    </Card>
     ); 
 }
